69d22a4aec21b60455c632ede91ba63fe4834eb1
JacobsKehl <henrique.j.kehl@icloud.com>
2026-05-16 16:41:16 -0300

    fix_connection_pool_errors

--- src/app/flashcards/page.tsx
+++ src/app/flashcards/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+export const dynamic = "force-dynamic";
 import { Logo } from "@/components/ui/logo";
 import { prisma } from "@/lib/prisma";
 import { getMockUserId } from "@/lib/auth-mock";
